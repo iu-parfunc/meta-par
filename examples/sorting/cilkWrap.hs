15d7d9802132f5509a6e1e5da8a462e9e4ffe530
Sajith T S <sajith@nonzen.in>
2012-03-04 18:00:33 -0500

    delete trailing whitespace.

--- examples/sorting/cilkWrap.hs
+++ examples/sorting/cilkWrap.hs
@@ -8,7 +8,7 @@ import Data.List
 import System.CPUTime (getCPUTime)
 import Text.Printf
 
-import qualified Data.Vector.Unboxed as V 
+import qualified Data.Vector.Unboxed as V
 import qualified Data.Vector.Unboxed.Mutable as MV
 import Control.Exception
 
@@ -16,22 +16,22 @@ foreign import ccall unsafe "wrap_cilksort"
   c_cilksort ::  Ptr CLong -> Ptr CLong -> CLong -> IO CLong
 
 foreign import ccall unsafe "run_cilksort"
-  c_run_cilksort :: CLong -> IO CLong 
+  c_run_cilksort :: CLong -> IO CLong
 
 main :: IO ()
-main = 
-  do args <- getArgs 
+main =
+  do args <- getArgs
      let (size,runs) = case args of
            []    -> (300000, 1)
            [m]   -> (read m, 1)
            [m,n] -> (read m, read n)
-                
+
      printf "Running cilksort with a size of %d (foreign array) \
         \ (%d runs)\n" (fromIntegral size :: Int) (runs :: Int)
 
      runCilkSort size runs
 
-     printf "Running cilksort with a size of %d (Haskell array): \ 
+     printf "Running cilksort with a size of %d (Haskell array): \
                 \ (%d runs).\n" (fromIntegral size :: Int) (runs :: Int)
 
      -- TODO: would like to do this
@@ -41,7 +41,7 @@ main =
 
      let a = randomList (fromIntegral size) seed
          b = [1..size]
-       in 
+       in
         withArray a $ \pa ->
         withArray b $ \pb ->
         runCilkSort' (castPtr pa) (castPtr pb) size runs
@@ -65,16 +65,16 @@ randomList n = take n . unfoldr (Just . random)
 
 -- Create a vector containing the numbers [0,N) in random order.
 randomPermutation :: Int -> StdGen -> V.Vector Int
-randomPermutation len rng = 
+randomPermutation len rng =
   -- Annoyingly there is no MV.generate:
   V.create (do v <- V.unsafeThaw$ V.generate len id
                loop 0 v rng)
   -- loop 0 (MV.generate len id)
- where 
+ where
   loop n vec g | n == len  = return vec
-	       | otherwise = do 
+	       | otherwise = do
     let (offset,g') = randomR (0, len - n - 1) g
---    MV.unsafeSwap vec n 
+--    MV.unsafeSwap vec n
     MV.swap vec n (n + offset)
     loop (n+1) vec g'
 
